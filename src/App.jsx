fee6230e6fa121f420453a714d5fe94528865381
Tamil Selvan K <tamildeveloper2007@gmail.com>
2025-12-31 11:32:34 +0530

    Routing fallback corrections

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,21 @@ import WriteExam from "./components/WriteExam.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 import Login from "./components/Login.jsx";
-import Signup from './components/Signup.jsx'
+import Signup from "./components/Signup.jsx";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="site-container">
-      <AuthProvider>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route exact path="/login" element={<Login />} />
-            <Route excat path="/signup" element={<Signup />} />
-            <Route exact path="/" element={<Home />} />
-            <Route path="/dashboard/admin" element={<AdminDashboard />} />
-            <Route path="/dashboard/student" element={<StudentDashboard />} />
-            <Route path="/exam/write/:examId" element={<WriteExam />} />
-            <Route path="*" element={<Navigate to='/' replace />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
-    </div>
+    <Routes>
+      <Route exact path="/login" element={<Login />} />
+      <Route excat path="/signup" element={<Signup />} />
+      <Route exact path="/" element={<Home />} />
+      <Route path="/dashboard/admin" element={<AdminDashboard />} />
+      <Route path="/dashboard/student" element={<StudentDashboard />} />
+      <Route path="/exam/write/:examId" element={<WriteExam />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
+import "./App.css";
+
 import App from "./App.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
+import Header from "./components/Header.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <div className="site-container">
+      <AuthProvider>
+        <BrowserRouter>
+          <Header />
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </div>
   </StrictMode>
 );
